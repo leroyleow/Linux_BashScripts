c94619d04372929bbb95be7d24d22e6c436a2e26
leow cheekeong <leowcheekeong@leows-Mac-mini.local>
2022-12-10 10:04:02 +0800

    Inital Commit

---
+++ deploy_LAMP.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Code convention reference -> https://google.github.io/styleguide/shellguide.html
+# Script intent is to setup and LAMP [Linux Apache MongoDB Php] skeleton to Centos 
